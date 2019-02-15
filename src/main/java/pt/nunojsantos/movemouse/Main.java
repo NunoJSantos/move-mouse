699a9181274c438f196e6e72dc19725632ca7d01
Nuno Santos <nunojsantos@hotmail.com>
2019-02-15 14:55:07 +0000

    feat: time interval as argument

--- src/main/java/pt/nunojsantos/movemouse/Main.java
+++ src/main/java/pt/nunojsantos/movemouse/Main.java
@@ -6,16 +6,37 @@ import java.util.Random;
 
 public class Main {
 
-	private static final int THIRTY_SECONDS = 30000;
 	private static final int MAX_Y = 400;
 	private static final int MAX_X = 400;
+	private static int millisecondsInterval = 30000;
 
 	public static void main(String... args) throws AWTException, InterruptedException {
+
+		switch (args.length) {
+			case 0:
+				moveMouse();
+				break;
+			case 1:
+				try {
+					millisecondsInterval = Integer.parseInt(args[0]);
+					moveMouse();
+				} catch (NumberFormatException e) {
+					System.err.println("Bad usage");
+				}
+				break;
+			default:
+				System.err.println("Bad Usage");
+				break;
+		}
+
+	}
+
+	private static void moveMouse() throws AWTException, InterruptedException {
 		Robot robot = new Robot();
 		Random random = new Random();
 		while (true) {
 			robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
-			Thread.sleep(THIRTY_SECONDS);
+			Thread.sleep(millisecondsInterval);
 		}
 	}
 
